97f3d742ddf78c6d8c2398cc5779d4ffc9c1cc46
lotlouischoitslab <louis.s.cho@gmail.com>
2024-07-24 16:04:38 -0500

    add

--- conv-social-pooling/codes/louis.py
+++
@@ -1,7 +0,0 @@
-import torch
-import pandas as pd 
-print("CUDA available:", torch.cuda.is_available())
-print("PyTorch version:", torch.__version__)
-print("CUDA version:", torch.version.cuda)
- 
-
