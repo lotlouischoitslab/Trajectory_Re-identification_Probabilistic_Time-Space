29375ef001ae2fd85e79b1a42b038a405a1f27bf
lotlouischoitslab <louis.s.cho@gmail.com>
2024-07-29 15:08:53 -0500

    fixed plots

--- conv-social-pooling/codes/plot_trajectories.py
+++ conv-social-pooling/codes/plot_trajectories.py
@@ -1,28 +1,24 @@
 import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt 
+import pandas as pd
+import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 
 def plot_original_trajectories():
     lane = -2
     file_to_read = 'I294_Cleaned.csv'  # TGSIM csv dataset 
-    df = pd.read_csv(file_to_read) # read in the data 
-    input_data = df.copy() # copy the dataframe  
-    overpass_start_loc_x, overpass_end_loc_x = 1800, 1815 # both in meters Overpass width 17 meters (56 feet) 
-    overpass_length = overpass_end_loc_x - overpass_start_loc_x # length of the overpass
+    df = pd.read_csv(file_to_read)  # Read in the data 
+    input_data = df.copy()  # Copy the dataframe  
+    overpass_start_loc_y, overpass_end_loc_y = 1800, 1815  # Both in meters Overpass width 15 meters (50 feet)  
     input_data = input_data[input_data['lane'] == lane].reset_index(drop=True)  # Filter data for the given lane
-    incoming_trajectories = input_data[input_data['xloc'] <= overpass_start_loc_x] # Incoming trajectory before overpass  
-    outgoing_trajectories_temp = input_data[(input_data['xloc'] >= overpass_end_loc_x) & (input_data['xloc'] <= overpass_end_loc_x + overpass_length)] # Groundtruth trajectory after the overpass  
-    possible_trajectories = input_data[(input_data['xloc'] >= overpass_end_loc_x) & (input_data['xloc'] <= overpass_end_loc_x + overpass_length)] # All possible trajectories that we need to consider
-
-    outgoing_trajectories = input_data[input_data['xloc'] >= overpass_end_loc_x] # Groundtruth trajectory after the overpass  
+    incoming_trajectories = input_data[input_data['xloc'] <= overpass_start_loc_y]  # Incoming trajectory before overpass  
+    outgoing_trajectories = input_data[input_data['xloc'] >= overpass_end_loc_y]  # Groundtruth trajectory after the overpass  
     incoming_IDs = incoming_trajectories['ID'].unique()
     outgoing_IDs = outgoing_trajectories['ID'].unique()
 
     axis_coordinates = ['xloc', 'yloc']
 
     for axis_temp in axis_coordinates:
-        fig, ax = plt.subplots() # get xs and ts of each vehicle
+        fig, ax = plt.subplots(figsize=(120, 40))  # get xs and ts of each vehicle
 
         for i in incoming_IDs:
             temp_data = incoming_trajectories[incoming_trajectories['ID'] == i]
@@ -31,7 +27,7 @@ def plot_original_trajectories():
             speeds = temp_data['speed'].to_numpy()
             norm = Normalize(vmin=speeds.min(), vmax=speeds.max())  # Normalize speed values
             colors = plt.cm.viridis(norm(speeds))  # Use the 'viridis' colormap for more diverse colors
-            ax.scatter(ts, ys, s=1, c=colors) 
+            scatter = ax.scatter(ts, ys, s=1, c=colors)
 
         for j in outgoing_IDs:
             temp_data = outgoing_trajectories[outgoing_trajectories['ID'] == j]
@@ -40,62 +36,29 @@ def plot_original_trajectories():
             speeds = temp_data['speed'].to_numpy()
             norm = Normalize(vmin=speeds.min(), vmax=speeds.max())  # Normalize speed values
             colors = plt.cm.viridis(norm(speeds))  # Use the 'viridis' colormap for more diverse colors
-            ax.scatter(ts, ys, s=1, c=colors)
+            scatter = ax.scatter(ts, ys, s=1, c=colors)
+
+        # Highlight the missing gap
+        ax.axhspan(overpass_start_loc_y, overpass_end_loc_y, color='black', alpha=0.3, label='Overpass')
+
+        # Adding a color bar
+        cbar = fig.colorbar(scatter, ax=ax, fraction=0.046, pad=0.04)
+        cbar.set_label('Speed (m/s)', fontsize=50)
+        cbar.ax.tick_params(labelsize=50)  # Increase color bar tick size
 
         if axis_temp == 'xloc':
             ax.set_xlim(0, 320)
             ax.set_ylim(1000, 2200)  # Set y-axis range from 1000 to 2200
 
-        ax.set_xlabel('Time (s)', fontsize=20)
-        ax.set_ylabel('Location (m)', fontsize=20)
-        ax.xaxis.set_major_locator(plt.MaxNLocator(100)) # Increase the number of grid lines on the x-axis 
-        ax.yaxis.set_major_locator(plt.MaxNLocator(60)) # Increase the number of grid lines on the y-axis
-        ax.grid()
+        ax.set_xlabel('Time (s)', fontsize=50)
+        ax.set_ylabel('Location (m)', fontsize=50)
+        ax.tick_params(axis='both', which='major', labelsize=50) 
 
-        fig.set_size_inches(80, 30)
-        fig.savefig(f'trajectory_plots/trajectory-{axis_temp}.png')
+        fig.savefig(f'trajectory_plots/trajectory-{axis_temp}.png') # Save the figure 
 
-
-def plot_raw_trajectories():
-    smoothed_file = pd.read_csv('I294_Cleaned.csv') # read in the data 
-    lanes = sorted(smoothed_file['lane'].unique()) # lanes to analyze
-
-    for lane in lanes:
-        lane_data = smoothed_file[smoothed_file['lane']==lane].reset_index(drop=True)
-
-        IDs = []
-        all_ts = []
-        all_ys = []
-        init_ID = -1
-
-        # get all vehicle IDs
-        for i in range(len(lane_data)):
-            if lane_data['ID'][i] != init_ID:
-                IDs.append(lane_data['ID'][i])
-                init_ID = lane_data['ID'][i]
-
-        # get xs and ts of each vehicle
-        fig, ax = plt.subplots()
-        for j in IDs:
-            temp_data = lane_data[lane_data['ID']==j]
-            ys = temp_data['xloc'].to_numpy()
-            #ys = temp_data['yloc'].to_numpy()
-            ts = temp_data.time.to_numpy()
-            ax.scatter(ts, ys,s=1)
-            ax.text(ts[0], ys[0], str(j))
-
-        ax.set_xlabel('Time (s)', fontsize = 20)
-        ax.set_ylabel('Location (m)', fontsize = 20)
-        ax.xaxis.set_major_locator(plt.MaxNLocator(100)) # Increase the number of grid lines on the x-axis 
-        ax.yaxis.set_major_locator(plt.MaxNLocator(60)) # Increase the number of grid lines on the y-axis
-        ax.grid()
-
-        fig.set_size_inches(120,30)
-        fig.savefig(f'lane_plots/Lane_{lane}-x.png')
 
 def main():
     plot_original_trajectories()
-    plot_raw_trajectories()
 
 if __name__ == '__main__':
     main()
